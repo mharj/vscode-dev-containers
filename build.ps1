618321640357b2d3162b9a110f043f8b6e75a479
Marko Harjula <marko.harjula@avanio.com>
2024-03-28 13:00:58 +0200

    add php 8.2

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 #$nodeVersions = [System.Collections.ArrayList]@(14,16,18);
-$nodeVersions = [System.Collections.ArrayList]@(16,18,20);
+$nodeVersions = [System.Collections.ArrayList]@(16, 18, 20);
 
-$phpVersions = [System.Collections.ArrayList]@("7.1","7.4","8.1");
+$phpVersions = [System.Collections.ArrayList]@("7.1", "7.4", "8.1", "8.2");
 
 # initialize
 docker pull ubuntu:focal
@@ -10,22 +10,20 @@ docker pull ubuntu:focal
 docker build -t mharj/vscode-base -f containers/vscode-base/Dockerfile .
 docker push mharj/vscode-base
 
-foreach ($nodeVersion in $nodeVersions)
-{
+foreach ($nodeVersion in $nodeVersions) {
     # build and push nodejs image
     $dockerFile = "containers/vscode-apache-nodejs/Dockerfile"
     $imageName = "mharj/vscode-apache-nodejs:$nodeVersion"
     docker build --build-arg NODE_VERSION=$nodeVersion -t $imageName -f $dockerFile .
-    if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
     docker push $imageName
 
     # build and push all php images from the nodejs image
-    foreach ($phpVersion in $phpVersions)
-    {
+    foreach ($phpVersion in $phpVersions) {
         $dockerFile = "containers/vscode-php/$phpVersion/Dockerfile"
         $imageName = "mharj/vscode-php:node$nodeVersion-php$phpVersion"
         docker build --build-arg NODE_VERSION=$nodeVersion -t $imageName -f $dockerFile .
-        if($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+        if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
         docker push $imageName
     }
 }
